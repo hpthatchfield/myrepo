469a342cd73d34ded154b27531f2d1cbbc50cc25
H Perry Hatchfield <hperryt@gmail.com>
2018-11-06 14:31:00 -0600

    initialized hello world repo

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 
+
 def do_something():
-    something = "Hello world!"
+    something = "¯\_(ツ)_/¯"
     print(something)# this will make it much easier in future problems to see that something is actually happening
